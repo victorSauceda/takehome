9075cfbee6a27c7b64c093559404a774a9e5918e
Victor <victorasauceda@gmail.com>
2020-09-14 02:24:42 -0700

    Clean Up

--- src/App.js
+++ src/App.js
@@ -1,44 +1,7 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React, { useState } from "react";
-import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-// import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import { withStyles } from "@material-ui/core/styles";
 
 function App() {
   return (
@@ -113,8 +76,6 @@ function App() {
           />
         </Grid>
         <Grid item xs={12}>
-          {/* 
-        const zipCodeRegex = /^\d{5}$/; */}
           <TextField
             variant="outlined"
             required
@@ -133,13 +94,8 @@ function App() {
             fullWidth
             type="email"
             id="email"
-            // value={this.state.email}
-            // onChange={(event) =>
-            //   this.setState({ email: event.target.value })
-            // }
             label="Email Address"
             name="email"
-            required
           />
         </Grid>
         <Grid item xs={12}>
@@ -151,7 +107,6 @@ function App() {
             id="phone"
             label="Phone Number"
             name="phone"
-            required
           />
         </Grid>
 
